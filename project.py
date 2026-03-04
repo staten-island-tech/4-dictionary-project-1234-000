ba12f22ad8c5417b9defbfb3af21f7dd0607c348
1234-000 <wilsonm68@nycstudents.net>
2026-03-04 08:43:04 -0500

    1

--- project.py
+++ project.py
@@ -1,2 +1,9 @@
-store = {
-    
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
+
+print(item)
+
--- t.py
+++ t.py
@@ -1,3 +1,10 @@
-def greet(name):
-    print(f"Hello, {name}!")
-greet("A")
+block = input("Input: ").upper()
+c1 = "H"
+c2 = "O"
+c3 = "N"
+c4 = "I"
+
+count = int(block.count(c1))
+count2 = int(block.count(c2))
+count3 = int(block.count(c3))
+count4 = int(block
