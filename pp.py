98b41dd858456a2eb9e18a439a18acddc7022ae8
1234-000 <wilsonm68@nycstudents.net>
2026-03-09 09:24:15 -0400

    Update pp.py

--- pp.py
+++ pp.py
@@ -16,4 +16,32 @@ print(item[0]["name"],item[0]["price"],item[0]["department"])
 print(item[1]["name"],item[1]["price"],item[1]["department"])
 print(item[2]["name"],item[2]["price"],item[2]["department"])
 
-buyer = input("Buy What? ")
+buyer = input("Buy What? ").upper()
+
+if buyer == "SAMSUNG 55":
+    print("Samsung 55")
+elif buyer == "IPHONE 18":
+    print("Iphone 18")
+elif buyer == "IPHONE 20":
+    print("Iphone 20")
+
+while True:
+    user = input("Do you want to continue? yes/no ").lower()
+    if user == "no":
+        break
+    else:
+        if user == "yes":
+            buy2 = input("What Else?")
+            if buy2 == "SAMSUNG 55":
+                print("Samsung 55")
+                print(buyer)
+            elif buy2 == "IPHONE 18":
+                print("Iphone 18")
+                print(buyer)
+            elif buy2 == "IPHONE 20":
+                print("Iphone 20")
+                print(buyer)
+
+
+
+
