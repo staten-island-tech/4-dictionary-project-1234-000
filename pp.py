6ef434f032b9a74c0d299f82720f34d82da56ff7
1234-000 <wilsonm68@nycstudents.net>
2026-03-16 09:06:13 -0400

    qwwert

--- pp.py
+++ pp.py
@@ -1,5 +1,4 @@
-def buy2():
-    print(buy2)
+
 item =[
     {"name": "Samsung 55", 
      "price": "$429.99", 
@@ -19,6 +18,7 @@ print(item[1]["name"],item[1]["price"],item[1]["department"])
 print(item[2]["name"],item[2]["price"],item[2]["department"])
 
 price = 0
+count = 0
 
 buyer = input("Buy What? ").upper()
 if buyer == "SAMSUNG 55":
@@ -30,29 +30,28 @@ elif buyer == "IPHONE 18":
 elif buyer == "IPHONE 20":
     print("Iphone 20")
     price += int(1000)
-
 while True:
     user = input("Do you want to continue? yes/no ").lower()
     if user == "no":
         print(buyer)
+        if count >= 1:
+            print(buy2)
         print("$", price)
-        print(buy2)
         break
     else:
         if user == "yes":
+            count += 1
             buy2 = input("What Else?").upper()
             if buy2 == "SAMSUNG 55":
                 print("Samsung 55")
                 price += float(429.99)
-                print("$",price)
             elif buy2 == "IPHONE 18":
                 print("Iphone 18")
                 price += int(123)
-                print("$",price)
             elif buy2 == ("IPHONE 20"):
                 print("Iphone 20")
                 price += int(1000)
-                print("$",price)
+
 
 
 
